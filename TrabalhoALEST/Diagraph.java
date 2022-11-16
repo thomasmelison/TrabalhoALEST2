2881bd7f4a4323b1449e43512ef1cb1f1c5e2340
Thomas Mello <thomasmelison@hotmail.com>
2022-11-15 23:16:27 -0300

    Terminando função de adicionar todos os subfilhos

--- TrabalhoALEST/Diagraph.java
+++ TrabalhoALEST/Diagraph.java
@@ -4,17 +4,90 @@ import java.util.ArrayList;
  * Diagraph
  */
 public class Diagraph {
+    public static int soma = 0;
+    public ArrayList<Node> nodos;
 
-    public Node[] nodos;
+    public Diagraph(){
+        nodos = new ArrayList<Node>();
+    }
 
 
-    class Node{
-        private String sabor;
-        private ArrayList<Node> filhos; 
+    public class Node{
+        public String sabor;
+        public ArrayList<Node> filhos; 
 
         public Node(String sabor){
             this.sabor = sabor;
             filhos = new ArrayList<>();
         }
+    }
+
+    public Node procuraSaborExistente(String saborInput){
+        for(Node nodo : nodos){
+            if(nodo.sabor.equals(saborInput)){
+                return nodo;
+            }
+        }
+        return null;
+    }
+
+    public Node criaNode(String sabor){
+        Node nodo = new Node(sabor);
+        nodos.add(nodo);
+        return nodo;
+    }
+
+    public void adicionaFilho(Node nodoPai, Node nodoFilho){
+        nodoPai.filhos.add(nodoFilho);
+    }
+
+    public boolean testePaternidadeRatinho(Node nodoPai, Node possivelFilho){
+        for(Node nodo : nodoPai.filhos){
+            if(nodo.equals(possivelFilho)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    private int getCombinacoesDoisSabores(Node[] nodes){
+        int combinacoesDoisSabores = 0;
+        for(int i = 0; i < nodes.length; i++){
+            combinacoesDoisSabores += nodes[i].filhos.size();
+        }
+
+        return combinacoesDoisSabores;
+    }
+
+    private int getCombinacoesTresSabores(Node[] nodes){
+        int combinacoesTresSabores = 0;
+        for(int i = 0; i < nodes.length; i++){
+            ArrayList<Node> filhosNodo = nodes[i].filhos;
+            for(Node n : filhosNodo){
+                combinacoesTresSabores += n.filhos.size();
+            }
+        }
+
+        return combinacoesTresSabores;
+    }
+
+    public void adicionaSubFilhos(){
+        for(Node nodoPai : nodos){
+            ArrayList<Diagraph.Node> filhosNodo = (ArrayList<Diagraph.Node>) nodoPai.filhos.clone();  
+            for(Node nodoFilho : filhosNodo){  
+                checaFilhosRecursivo(nodoPai , nodoFilho);
+            }
+        }
+    }
+
+    private void checaFilhosRecursivo(Node nodoPai, Node nodoFilho) {
+        if(!testePaternidadeRatinho(nodoPai, nodoFilho)){ 
+            nodoPai.filhos.add(nodoFilho);
+        }
+        for(Node nodoNeto : nodoFilho.filhos){
+          
+            checaFilhosRecursivo(nodoPai, nodoNeto);
+        }
+        return;
     }
 }
--- TrabalhoALEST/Main.java
+++ TrabalhoALEST/Main.java
@@ -1,3 +1,59 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
 public class Main {
     
+
+
+    public static void main(String[] args) throws IOException{
+        
+
+
+        Scanner scanner = new Scanner(new File("casocohen10.txt"));
+        Diagraph diagraph = new Diagraph();
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            if(!line.contains("->")){
+                break;
+            }
+            String[] sabores = line.split("->");
+            sabores[0] = sabores[0].replaceAll(" ", "");
+            sabores[1] = sabores[1].replaceAll(" ", "");
+
+            Diagraph.Node nodoPai = diagraph.procuraSaborExistente(sabores[0]);
+            //System.out.println(nodoPai.sabor);
+            if(nodoPai == null){
+                // cria nodo
+                nodoPai = diagraph.criaNode(sabores[0]);
+            }
+
+            Diagraph.Node nodoFilho = diagraph.procuraSaborExistente(sabores[1]);
+            if(nodoFilho == null){
+                // cria nodo
+                nodoFilho = diagraph.criaNode(sabores[1]);
+            }
+
+            // cria ligação
+            diagraph.adicionaFilho(nodoPai, nodoFilho);
+        }
+        scanner.nextLine();
+        diagraph.adicionaSubFilhos();
+        /* 
+        for(Diagraph.Node nodo : diagraph.nodos){
+            System.out.println("Nodo pai: " + nodo.sabor);
+            System.out.print("Filhos : ");
+            for(Diagraph.Node nodofilho : nodo.filhos){
+                System.out.print(nodofilho.sabor + " ");
+            }
+            System.out.println();
+            System.out.println();
+        } */
+
+        
+
+    }
 }
